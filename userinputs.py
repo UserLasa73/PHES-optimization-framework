ac2b5386d8593029c12c50448066f8722c4f1486
Lasantha Kumara <lasanthakumaraofficial@gmail.com>
2026-07-03 15:16:44 +0530

    created separate file for defining user inputs clearly

---
+++ userinputs.py
@@ -0,0 +1,33 @@
+# user_inputs.py
+# All user inputs as defined in proposal
+
+class UserInputs:
+    def __init__(self):
+        # Site
+        self.location = "Vavuniya"
+        self.latitude = 8.9
+        self.longitude = 79.9
+        
+        # Solar PV
+        self.pv_kwp = 30.0
+        self.tilt_angle = 10.0
+        self.azimuth_angle = 0.0
+        
+        # Load
+        self.daily_energy_kwh = 50.0
+        self.load_csv_path = None  # Optional
+        
+        # Physical
+        self.head_m = None  # None = auto-optimize (10-30m)
+        self.upper_reservoir_type = "new_tank"
+        self.lower_reservoir_type = "new_tank"
+        
+        # Requirements
+        self.autonomy_days = 2.0
+        self.budget_lkr = None  # None = no budget constraint
+        
+        # Advanced
+        self.evaporation_rate_mm_month = 50.0  # Sri Lanka avg
+        self.pipe_roughness_m = 0.00015
+        self.demand_spike_factor = 1.0  # 1.0 = no spikes
+        self.has_grid_backup = False
